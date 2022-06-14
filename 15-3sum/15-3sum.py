e5991bd2167d689958aef6371834c52674e016eb
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-06-14 14:10:58 -0400

    Time: 1003 ms (42.56%), Space: 16.8 MB (42.58%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -3,35 +3,37 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
+        
+        [-4,-1,-1,0,1,2]
+        
         """
         
         nums.sort()
         res = []
         
-        for i in range(len(nums)-2):
-          
+        for i in range(0, len(nums)-2):
           if i != 0 and nums[i] == nums[i-1]:
             continue
-          
+            
           target = 0 - nums[i]
           
-          l = i+1
-          r = len(nums)-1
+          l, r = i+1, len(nums)-1
           
           while l < r:
-            if nums[l] + nums[r] == target:
-              res.append([nums[i], nums[l], nums[r]])
-              l+=1
-              while nums[l] == nums[l-1] and l < r:
-                l+=1
-              
-            if nums[l] + nums[r] > target:
-              r-=1
-            if nums[l] + nums[r] < target:
-              l+=1
-
-        return res
+            total = nums[l] + nums[r]
             
-        
-        
+            if total == target:
+              res.append([nums[i],nums[l],nums[r]])
+              l+=1
+              while l < r and nums[l] == nums[l-1]:
+                l+=1
+            elif total > target:
+              r-=1
+            elif total < target:
+              l+=1
+              
+        return res
+          
+          
+          
         
