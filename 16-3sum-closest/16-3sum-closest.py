e9c89e679c582d24ace343001ef1116464774df1
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-06-20 14:11:36 -0400

    Time: 7851 ms (5.05%), Space: 13.5 MB (42.01%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -11,21 +11,24 @@ class Solution(object):
         """
               
         nums.sort()
-        result = nums[0] + nums[1] + nums[2]
-        for i in range(len(nums) - 2):
-            j, k = i+1, len(nums) - 1
-            while j < k:
-                sum = nums[i] + nums[j] + nums[k]
-                if sum == target:
-                    return sum
-                
-                if abs(sum - target) < abs(result - target):
-                    result = sum
-                
-                if sum < target:
-                    j += 1
-                elif sum > target:
-                    k -= 1
+        res = sum(nums[:3])
+        for i in range(len(nums)):
+          
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+          
+            l, r = i+1, len(nums)-1
+            while l < r:
+                s = sum((nums[i], nums[l], nums[r]))
+                if abs(s-target) < abs(res-target):
+                    res = s
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else: # break early 
+                    return res
+        return res
             
         return result
           
