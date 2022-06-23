e6b27607fcb68904cd389b95804f58fc186b4c9c
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-06-23 18:07:50 -0400

    Time: 26 ms (64.96%), Space: 13.5 MB (39.32%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -5,31 +5,21 @@ class Solution(object):
         :rtype: None Do not return anything, modify nums in-place instead.
         """
         
-        red = 0
-        white = 0
-        blue = 0
-        for c in nums:
-          if c == 0:
-            red+=1
-          elif c == 1:
-            white+=1
-          else:
-            blue+=1
+        l, r = 0, len(nums)-1
+        i = 0
         
-        counter = 0
-        while red != 0:
-          nums[counter] = 0
-          red -= 1
-          counter+=1
-        while white != 0:
-          nums[counter] = 1
-          white -= 1
-          counter+=1
-
-        while blue != 0:
-          nums[counter] = 2
-          blue -= 1
-          counter+=1
+        while i <= r:
+          temp = nums[i]
+          if nums[i] == 0:
+            nums[i] = nums[l]
+            nums[l] = temp
+            l += 1
+          if nums[i] == 2:
+            nums[i] = nums[r]
+            nums[r] = temp
+            r-=1
+            i-=1
+          i+=1
 
             
         
