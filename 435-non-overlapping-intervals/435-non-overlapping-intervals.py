43611134d7b43cfeafe64349a33fe48518d6e381
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-01 11:53:11 -0400

    Time: 2867 ms (10.64%), Space: 59.8 MB (70.14%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -6,22 +6,23 @@ class Solution(object):
         
         [[0,2],[1,3],[2,4],[3,5],[4,6]]
         
+          ---
+           ---
+            ---
+             ---
+              ---
         """
-        
-        intervals.sort(key=lambda x:x[0])
-        
-        prev = intervals[0]
-        
         res = 0
-        
-        for curr in intervals[1:]:
-          if prev[1] > curr[0]:
+        intervals.sort(key=lambda x:x[0])
+        prev = intervals[0]
+        for i in range(1, len(intervals)):
+          curr = intervals[i]
+          if curr[0] < prev[1]:
             res += 1
             if curr[1] < prev[1]:
               prev = curr
           else:
             prev = curr
-        
-        return res
           
+        return res
         
