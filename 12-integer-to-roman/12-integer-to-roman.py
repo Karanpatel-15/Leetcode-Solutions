5951219ed0639dd579f5da7aa6ae658e8d784330
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-04 13:34:29 -0400

    Time: 69 ms (35.23%), Space: 13.4 MB (66.85%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+        order = [["M", 1000],["CM", 900],["D", 500],["CD", 400],
+                 ["C", 100],["XC", 90],["L", 50],["XL", 40],
+                 ["X", 10],["IX", 9],["V", 5],["IV", 4]]
+        res = ""
+        remainder = num
+        for l, v in order:
+          while v <= remainder:
+            res += l
+            remainder -= v
+          
+          if remainder < 4:
+            break
+        
+        for i in range(remainder):
+          res += "I"
+          
+        return res
