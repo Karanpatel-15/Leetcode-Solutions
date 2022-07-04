b44cf69d6c6f0d58ca48e7d91accef1e7ec4c67d
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-04 13:42:21 -0400

    Time: 71 ms (32.26%), Space: 13.5 MB (15.27%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -7,18 +7,11 @@ class Solution(object):
         
         order = [["M", 1000],["CM", 900],["D", 500],["CD", 400],
                  ["C", 100],["XC", 90],["L", 50],["XL", 40],
-                 ["X", 10],["IX", 9],["V", 5],["IV", 4]]
+                 ["X", 10],["IX", 9],["V", 5],["IV", 4], ["I", 1]]
         res = ""
         remainder = num
         for l, v in order:
           while v <= remainder:
             res += l
             remainder -= v
-          
-          if remainder < 4:
-            break
-        
-        for i in range(remainder):
-          res += "I"
-          
         return res
