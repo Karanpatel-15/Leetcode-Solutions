e56ff2534b8da1d39d6d50c17734609774b8d344
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-07 17:15:08 -0400

    Time: 46 ms (18.21%), Space: 13.5 MB (49.69%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def reorganizeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
+        count = {}
+        for l in s:
+          count[l] = count.get(l, 0) + 1
+          
+        maxHeap = []
+        for l in count:
+          maxHeap.append([-count[l], l])
+          
+        heapq.heapify(maxHeap)
+        
+        prev = None
+        res = ""
+        while maxHeap or prev:
+          if prev and not maxHeap:
+            return ""
+          
+          count, letter = heapq.heappop(maxHeap)
+          res += letter
+          count += 1
+          
+          if prev != None:
+            heapq.heappush(maxHeap, prev)
+          
+          if count != 0:
+            prev = [count, letter]
+          else:
+            prev = None
+            
+        return res
+          
+            
