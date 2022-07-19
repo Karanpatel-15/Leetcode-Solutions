4cdc0f874848a4a22b3d50c0fe51420cc062f8e6
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-19 13:51:25 -0400

    Time: 161 ms (19.28%), Space: 13.7 MB (35.55%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        
+        s1 = Counter(ransomNote)
+        s2 = Counter(magazine)
+        
+        
+        for l in ransomNote:
+          if l not in s2 or s2[l] < s1[l]:
+            return False
+          
+        return True
