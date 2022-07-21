7f35096c26b8eda3f5f78e115ef55f2b0e2ed179
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-21 11:21:02 -0400

    Time: 171 ms (23.83%), Space: 20.6 MB (29.72%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -12,17 +12,38 @@ class Solution(object):
         
         counter = Counter(nums)
         
-        heap = []
+        bucket = [[] for i in range(len(nums)+1)]
         
         for key in counter:
-          t = (-counter[key], key)
-          heapq.heappush(heap, t)
+          bucket[counter[key]].append(key)
           
         res = []
-        for i in range(k):
-          res.append(heapq.heappop(heap)[1])
-          
+        for i in range(len(bucket)-1, 0, -1):
+          if bucket[i] == []:
+            continue
+            
+          for j in bucket[i]:
+            if k == 0:
+              return res
+            res.append(j)
+            k-=1
+            
         return res
+          
+          
+        
+        
+        
+        # heap = []
+#         for key in counter:
+#           t = (-counter[key], key)
+#           heapq.heappush(heap, t)
+          
+#         res = []
+#         for i in range(k):
+#           res.append(heapq.heappop(heap)[1])
+          
+#         return res
         
         
         
