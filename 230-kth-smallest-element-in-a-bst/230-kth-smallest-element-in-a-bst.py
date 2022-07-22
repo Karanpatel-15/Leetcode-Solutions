9fd8c903110a395decf68317978522966e0f0f82
Karan Patel <84426799+Karanpatel-15@users.noreply.github.com>
2022-07-22 12:54:40 -0400

    Time: 67 ms (40.81%), Space: 20.9 MB (98.67%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -14,13 +14,17 @@ class Solution(object):
         
         stack = []
         while root or stack:
+          
             while root:
                 stack.append(root)
                 root = root.left
+                
             root = stack.pop()
             k -= 1
+            
             if k == 0:
                 return root.val
+              
             root = root.right
 
             
